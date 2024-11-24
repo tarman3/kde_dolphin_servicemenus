8a331a22de9d303fcb79a626b844e41dde3c14e5
tarman3 <joice501790018@tutamail.com>
2024-11-24 15:08:20 +0200

    Added pdf_export_pages

---
+++ pdf_export_pages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+old_ifs="$IFS"
+IFS=$';'
+read -r -a array <<< "$1"
+IFS="$old_ifs"
+
+firstFile=${array[0]}
+path=${firstFile%/*}
+
+parameters=`kdialog --title="Export pages from PDF" --inputbox "Pages number (1-3,5,6) or (5-z)" "1"`
+
+exit_status=$?
+if [ $exit_status != 0 ]; then exit; fi
+
+numberFiles=${#array[@]}
+dbusRef=`kdialog --title "Export pages from PDF" --progressbar "" $numberFiles`
+
+for file in "${array[@]}"; do
+
+    qpdf --decrypt --pages "$file" $parameters -- "$file" "${file%.*}_pages_${parameters}.pdf"
+
+    counter=$(($counter+1))
+    qdbus $dbusRef Set "" value $counter
+    qdbus $dbusRef setLabelText "Completed $counter of $numberFiles"
+
+done
+
+qdbus $dbusRef close
+
+kdialog --title "Export pages from PDF" --icon "checkbox" --passivepopup "Completed" 3
