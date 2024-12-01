d9d079b06c45a64f2cae5615c2dcd307cd82ba49
tarman3 <joice501790018@tutamail.com>
2024-12-01 10:50:20 +0200

    rename media_cut_2-2, cut parameters

--- media_cut_waste.sh
+++ media_cut_waste.sh
@@ -4,10 +4,14 @@ firstFile="$1"
 path=${firstFile%/*}
 ext=${firstFile##*.}
 
+cutStart=2
+cutFinish=2
 
 duration=`ffprobe -i "$firstFile" -show_entries format=duration -v quiet -of csv="p=0"`
 duration=${duration%.*}
 
-ffmpeg -v quiet -stats -i "$firstFile" -y -ss 2 -to $(($duration-2)) -strict -2 "${firstFile%.*}_2-2.$ext"
+finishTime=$(($duration-$cutFinish))
+
+ffmpeg -v quiet -stats -i "$firstFile" -y -ss $cutStart -to $finishTime -strict -2 "${firstFile%.*}_$cutStart-$finishTime.$ext"
 
 kdialog --title "Media Cut 2-2" --icon "checkbox" --passivepopup "Completed" 3
