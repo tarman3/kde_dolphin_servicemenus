08c2ba7ad5c5941f2819b189d3ce04498b1ea9e0
tarman3 <joice501790018@tutamail.com>
2024-12-01 10:34:08 +0200

    hash - open result in editor

--- hash.sh
+++ hash.sh
@@ -8,14 +8,16 @@ IFS="$old_ifs"
 firstFile=${array[0]}
 path=${firstFile%/*}
 
-parameters=`yad --borders=20 --width=300 --title="Hash Calculate" --item-separator="|" --separator="," --form \
-    --field="Type:CB" --field="Save hash in one file:CHK" "^md5|sha256|sha1|sha224|sha384|sha512" TRUE`
+parameters=`yad --borders=20 --width=300 --title="Hash Calculate" --item-separator="|" --separator="," \
+    --form --field="Type:CB" --field="Save hash in one file:CHK" --field="Open result in Editor:CHK" \
+    "^md5|sha256|sha1|sha224|sha384|sha512" TRUE TRUE`
 
 exit_status=$?
 if [ $exit_status != 0 ]; then exit; fi
 
 hashType=$( echo $parameters | awk -F ',' '{print $1}')
 merge=$( echo $parameters | awk -F ',' '{print $2}')
+open=$( echo $parameters | awk -F ',' '{print $3}')
 
 numberFiles=${#array[@]}
 dbusRef=`kdialog --title "Hash calculate" --progressbar "" $numberFiles`
@@ -27,6 +29,10 @@ for file in "${array[@]}"; do
     if [ "$merge" = TRUE ]
         then echo -e "$hashType    $hash    $file" >> "$firstFile.$hashType"
         else echo -e "$file\n$hashType\n$hash" > "$file.$hashType"
+    fi
+
+    if [ "$merge" = TRUE ] && [ "$open" = TRUE ]; then xdg-open "$firstFile.$hashType"
+    elif [ "$merge" = FALSE ] && [ "$open" = TRUE ]; then xdg-open "$file.$hashType"
     fi
 
     counter=$(($counter+1))
