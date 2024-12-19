8a19238946c13c6d557960ded0971d97eff94e7b
tarman3 <joice501790018@tutamail.com>
2024-12-19 12:10:43 +0200

    media_extract_audio - show notify than not audio tracks

--- media_extract_audio.sh
+++ media_extract_audio.sh
@@ -2,6 +2,14 @@
 
 audioFormat='aac'
 
+utilities=('ffmpeg')
+for utility in ${utilities[@]}; do
+    if ! command -v "$utility" 2>&1 >/dev/null; then
+        kdialog --title "$utility" --icon "error" --passivepopup "Not found" 3
+        exit 1
+    fi
+done
+
 oldIFS="$IFS"
 IFS=$';'
 read -r -a array <<< "$1"
@@ -14,8 +22,9 @@ dbusRef=`kdialog --title "Extract Audio" --progressbar "1 of $numberFiles  =>  $
 for file in "${array[@]}"; do
 
     amontAudioTracks=`ffprobe -v error -select_streams a -show_entries stream=index -of csv=p=0 "$file" | wc -w`
-    if [ "$amontAudioTracks" -ge 1 ]; then
-        ffmpeg -v error -i "$file" -vn -acodec copy "${file%.*}.${audioFormat}"
+    if [ "$amontAudioTracks" -ge 1 ]
+        then ffmpeg -v error -i "$file" -vn -acodec copy "${file%.*}.${audioFormat}"
+        else echo 1; kdialog --title "${file##*/}" --icon "error" --passivepopup "Does not contain audio tracks" 3
     fi
 
     counter=$(($counter+1))
