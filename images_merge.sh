81c0d2df6402cab6154830969213484225b500e1
tarman3 <joice501790018@tutamail.com>
2024-12-21 13:01:09 +0200

    images_merge - added dir to save

--- images_merge.sh
+++ images_merge.sh
@@ -15,7 +15,8 @@ read -r -a array <<< "$1"
 
 firstFile=${array[0]}
 path=${firstFile%/*}
-nameNoExt=${firstFile%.*}
+name=${firstFile##*/}
+nameNoExt=${name%.*}
 
 for file in "${array[@]}"; do
     ext=${file##*.}
@@ -30,8 +31,10 @@ fi
 parameters=`yad --borders=10 --width=400 --height=250 --title="Merge Images" \
     --item-separator="|" --separator="," --form  \
     --field="Direction:CB" --field="Space between images (px)" --field="Frame around images:CHK" \
-    --field="Background color:CB" --field="Format:CB" --field="Lossy compress:CHK" --field="Help:LINK" \
-    "^hor|vert" 5 FALSE "transparent|white|black" "$extForm" TRUE "https://imagemagick.org/script/montage.php"`
+    --field="Background color:CB" --field="Format:CB" --field="Lossy compress:CHK" \
+    --field="Dir to save:DIR" --field="Help:LINK" \
+    "^hor|vert" 5 FALSE "transparent|white|black" "$extForm" TRUE "$path" \
+    "https://imagemagick.org/script/montage.php"`
 
 exit_status=$?; if [ $exit_status != 0 ]; then exit; fi
 
@@ -41,8 +44,9 @@ frame=$(echo $parameters | awk -F ',' '{print $3}')
 backgroundColor=$(echo $parameters | awk -F ',' '{print $4}')
 ext=$(echo $parameters | awk -F ',' '{print $5}')
 compress=$(echo $parameters | awk -F ',' '{print $6}')
+dir=$( echo $parameters | awk -F ',' '{print $7}')
 
-newName="${nameNoExt}_$direction.$ext"
+newPath="${dir}/${nameNoExt}_$direction.$ext"
 
 numberFiles=${#array[@]}
 
@@ -52,13 +56,13 @@ if [ $direction = "vert" ]
 fi
 
 if [ "$frame" = TRUE ]
-    then montage -background $backgroundColor -geometry $opt1 -tile $opt2 `echo "$1"` $ext: | montage - -background $backgroundColor -geometry $opt1 "$newName"
-    else montage -background $backgroundColor -geometry $opt1 -tile $opt2 `echo "$1"` $ext: | magick - -shave $space $newName
+    then montage -background $backgroundColor -geometry $opt1 -tile $opt2 `echo "$1"` $ext: | montage - -background $backgroundColor -geometry $opt1 "$newPath"
+    else montage -background $backgroundColor -geometry $opt1 -tile $opt2 `echo "$1"` $ext: | magick - -shave $space $newPath
 fi
 
 if [ "$compress" = TRUE ]; then
-    if [ "${ext,,}" = 'png' ]; then pngquant --strip --force --quality=60-80 "${newName}" --output "${newName}"
-    elif [ "${ext,,}" = 'jpg' ]; then jpegoptim --strip-all --overwrite --max=75 "$newName"
+    if [ "${ext,,}" = 'png' ]; then pngquant --strip --force --quality=60-80 "$newPath" --output "$newPath"
+    elif [ "${ext,,}" = 'jpg' ]; then jpegoptim --strip-all --overwrite --max=75 "$newPath"
     fi
 fi
 
