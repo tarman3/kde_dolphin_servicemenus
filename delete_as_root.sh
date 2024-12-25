574b273d8c3c444505be94a8a55363c9fec4ff29
tarman3 <joice501790018@tutamail.com>
2024-12-25 12:55:12 +0200

    delete_as_root - press enter to exit

--- delete_as_root.sh
+++ delete_as_root.sh
@@ -6,9 +6,12 @@ IFS=$';'
 for file in $1; do
 
     if [ -f "$file" ] || [ -d "$file" ]
-        then sudo rm --recursive "$file"
+        then sudo rm --force --recursive --verbose "$file"
     fi
 
 done
 
-kdialog --title "Delete as root" --icon "checkbox" --passivepopup "Completed" 3
+echo
+read -p "Press Enter to exit"
+
+# kdialog --title "Delete as root" --icon "checkbox" --passivepopup "Completed" 3
